2a9560c8e829868273b258d605f96bab1f5fefa0
Michele Marschner <michele.marschner@gmail.com>
2026-02-15 08:31:12 +0100

    deleted all imports for in progress modules

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import argparse
 from pathlib import Path
 
 from experiments.run_experiments import run_experiments
-from analysis.run_analysis import run_analysis
+#from analysis.run_analysis import run_analysis
 
 
 def main() -> None:
@@ -19,17 +19,17 @@ def main() -> None:
         
         run_experiments(Path(args.arg1), args.overwrite)
 
-    if args.action == "evaluate":
-        if not args.arg1:
-            raise SystemExit("evaluation requires arg1 = experiment_id")
+    #if args.action == "evaluate":
+    #    if not args.arg1:
+    #        raise SystemExit("evaluation requires arg1 = experiment_id")
         
-        run_analysis(Path(args.arg1))
+    #    return
 
-    if args.action == "analyse":
-        if args.arg1 is None:
-            run_analysis()  
-        else:
-            run_analysis(Path(args.arg1))
+    #if args.action == "analyse":
+    #    if args.arg1 is None:
+    #        run_analysis()  
+    #    else:
+    #        run_analysis(Path(args.arg1))
     
 if __name__ == "__main__":
     main()
