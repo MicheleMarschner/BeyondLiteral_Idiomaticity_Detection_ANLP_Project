1e99660452240f23c2cca2e3361ddcefc19f4d50
Michele Marschner <michele.marschner@gmail.com>
2026-02-19 16:39:31 +0100

    created example for transformer integration into wrapper ModelRunner

---
+++ src/models/dummyBERT/BERTRunner.py
@@ -0,0 +1,217 @@
+
+from typing import Dict, Tuple, Any, List
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+from pathlib import Path
+import pandas as pd
+from transformers import (AutoModelForSequenceClassification, AutoTokenizer, PreTrainedModel, Trainer, TrainingArguments)
+from sklearn.metrics import f1_score
+from datasets import Dataset
+
+from utils.helper import set_seeds
+
+
+def tokenize_function(examples, hyper_params):
+    """Tokenize text with padding and truncation"""
+
+    max_length = hyper_params.get("max_length")
+    
+    MODEL_NAME = "bert-base-multilingual-cased"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    return tokenizer(
+        examples["text"],
+        padding="max_length",
+        truncation=True,
+        max_length=max_length
+    )
+
+
+def tokenize_input(hyper_params, train_data, dev_data):
+    train_dataset = Dataset.from_pandas(train_data)
+    dev_dataset = Dataset.from_pandas(dev_data)
+    
+    train_dataset = train_dataset.map(tokenize_function, hyper_params, batched=True)
+    dev_dataset = dev_dataset.map(tokenize_function, hyper_params, batched=True)
+
+    train_dataset.set_format("torch", columns=["input_ids", "text", "label"])
+    dev_dataset.set_format("torch", columns=["input_ids", "text", "label"])
+
+    return train_dataset, dev_dataset
+
+
+def compute_metrics(eval_pred):
+    """Calculate Macro F1 score (average of F1 for each class)"""
+    predictions, labels = eval_pred
+    predictions = predictions.argmax(axis=-1)
+    macro_f1 = f1_score(labels, predictions, average="macro")
+    return {"macro-F1": macro_f1}
+
+
+class BERTRunner:
+    def prepare_features(self, 
+        hyper_params: Dict[str, Any], 
+        exp_config: Dict[str, Any], 
+        train_df: pd.DataFrame,
+        test_df: pd.DataFrame
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Fit a featurizer on train text and transform train/test into feature matrices"""
+        
+        set_seeds(exp_config['seed'])
+        train_dataset, dev_dataset = tokenize_input(hyper_params, train_df, test_df)
+        
+        return train_dataset, dev_dataset
+
+    
+    def initialize(self, params: Dict[str, Any], seed: int, model_family: str) -> PreTrainedModel:
+        
+        model = AutoModelForSequenceClassification.from_pretrained(
+                    params.get("model_identifier"),
+                    num_labels=2,
+                    id2label={0: "Idiom", 1: "Literal"},
+                    label2id={"Idiom": 0, "Literal": 1},
+        )
+        return model
+    
+    
+    def tune(self, 
+        exp_config: Dict[str, Any],
+        model_path: Path,
+        train_df: pd.DataFrame,
+        dev_df: pd.DataFrame,
+        threshold: float=0.5
+    ) -> Tuple[PreTrainedModel, List[Dict[str, Any]], Dict[str, Any], Dict[str, Any]]:
+        """Grid-search hyperparameters, save the best model bundle, and return best model and other results"""
+        
+        out_dir = model_path / "training"
+        results = []
+        best_f1 = -1.0
+        best_curves = None
+        best_model = None
+        best_params = None
+        best_tokenizer = None
+
+        tok_space = {
+            "max_length": [64, 128, 256],
+        }
+        tok_grid = list(ParameterGrid(tok_space))
+
+
+        learning_space = {
+            "learning_rate": [5e-5, 2e-5],
+            "num_train_epochs": [3, 5],
+            "weight_decay": [0.0, 0.01],
+        }
+        learning_grid = list(ParameterGrid(learning_space))
+        MODEL_ID = "bert-base-multilingual-cased"
+
+        # Run through hyperparameter grid
+        for tokenization_config in tok_grid:
+            train_data, dev_data, tokenizer = self.prepare_features(params=tokenization_config, config=exp_config, train_df=train_df, test_df=dev_df)
+
+            for learning_config in learning_grid:
+                set_seeds(exp_config['seed'])
+                model = self.initialize(params={**learning_config, "model_identifier": MODEL_ID})
+                
+                run_name = (
+                    f"ml{tokenization_config['max_length']}"
+                    f"__lr{learning_config['learning_rate']}"
+                    f"__ep{learning_config['num_train_epochs']}"
+                    f"__wd{learning_config.get('weight_decay', 0.0)}"
+                    f"__seed{exp_config['seed']}"
+                )
+                run_dir = out_dir / run_name
+                run_dir.mkdir(parents=True, exist_ok=True)
+
+                training_args = TrainingArguments(
+                    output_dir=str(run_dir),
+
+                    # Hyperparameters (Baseline replication hence fixed)
+                    learning_rate=learning_config["learning_rate"],
+                    per_device_train_batch_size=32,
+                    per_device_eval_batch_size=32,
+                    num_train_epochs=learning_config["num_train_epochs"],
+                    weight_decay=learning_config["weight_decay"],
+
+                    # Evaluation & saving
+                    evaluation_strategy="epoch",
+                    save_strategy="epoch",
+                    save_total_limit=2,
+                    load_best_model_at_end=True,
+                    metric_for_best_model="macro-F1",
+
+                    # Reproducibility
+                    seed=exp_config['seed'],
+                    logging_dir=str(run_dir / "logs"),
+                    logging_steps=50,
+                    report_to=["none"]
+                )
+
+                trainer = Trainer(
+                    model=model,          
+                    args=training_args,                 
+                    train_dataset=train_data,
+                    eval_dataset=dev_data,
+                    tokenizer=tokenizer,
+                    compute_metrics=compute_metrics
+                )
+
+                trainer.train()
+                best_dev_f1 = float(trainer.state.best_metric) 
+
+                # saving the loss curves
+                train_steps, train_loss = [], []
+                eval_steps, eval_loss = [], []
+
+                for row in trainer.state.log_history:
+                    if "loss" in row and "step" in row:
+                        train_steps.append(row["step"])
+                        train_loss.append(row["loss"])
+                    if "eval_loss" in row and "step" in row:
+                        eval_steps.append(row["step"])
+                        eval_loss.append(row["eval_loss"])
+
+                loss_curves = {
+                    "train": {"step": train_steps, "loss": train_loss},
+                    "dev":  {"step": eval_steps,  "loss": eval_loss},
+                }
+
+                results.append({
+                    **tokenization_config,
+                    **learning_config,
+                    "best_dev_macro_f1": best_dev_f1,
+                })
+
+                if best_dev_f1 > best_f1:
+                    best_f1 = best_dev_f1
+                    best_model = trainer.model
+                    best_params = {**tokenization_config, **learning_config}
+                    best_tokenizer = tokenizer
+                    best_curves = loss_curves
+                
+        if best_model is None:
+            raise RuntimeError("Tuning failed: no devid parameter combination produced a trained model.")
+                
+
+        best_model_dir = Path(model_path / f"{exp_config['model_family']}")
+        best_model_dir.mkdir(parents=True, exist_ok=True)
+        best_model.save_pretrained(best_model_dir)
+        best_tokenizer.save_pretrained(best_model_dir)
+
+        return best_model, results, best_params, best_curves
+
+
+    def predict_proba(self, model, X: Any) -> np.ndarray:
+        """Wrapper to get positive-class probabilities from a model"""
+        
+        out = model.predict(X)
+        logits = out.predictions  
+
+        # softmax -> probabilities
+        logits = np.asarray(logits)
+        logits = logits - logits.max(axis=1, keepdims=True)  
+        probs = np.exp(logits) / np.exp(logits).sum(axis=1, keepdims=True)
+
+        proba_pos = probs[:, 1]   # class 1
+        
+        return proba_pos.astype(float)
