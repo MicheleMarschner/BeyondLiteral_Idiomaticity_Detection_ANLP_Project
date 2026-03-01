ae0d7c4a6fd3411624549ad9d72d362c05658665
Michele Marschner <michele.marschner@gmail.com>
2026-03-01 10:29:33 +0100

    fixed kaggle colab mismatch

--- src/models/BERTs/param_grid.py
+++ src/models/BERTs/param_grid.py
@@ -1,19 +1,5 @@
 mBERT_grid = {
     "tok_space": {
-        "max_length": [128],
-    },
-    "learning_space": {
-        "learning_rate": [2e-5],
-        "num_train_epochs": [3],
-        "weight_decay": [0.01],
-        "batch_size": [8],
-    }
-}
-
-
-""""
-mBERT_grid = {
-    "tok_space": {
         "max_length": [128, 256],
     },
     "learning_space": {
@@ -23,8 +9,6 @@ mBERT_grid = {
         "batch_size": [4, 8],
     }
 }
-
-"""
 
 
 
