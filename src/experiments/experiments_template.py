f6a0571084c1e054b32f5eddf19731e9a85fa8a3
Michele Marschner <michele.marschner@gmail.com>
2026-03-19 09:48:12 +0100

    Stop tracking wandb folder

--- src/experiments/experiments_template.py
+++ src/experiments/experiments_template.py
@@ -16,23 +16,24 @@ class ExperimentTemplate:
 EXPERIMENTS = ExperimentTemplate(
     settings=["zero_shot"],
     language_mode="per_language",
-    languages=["EN"],
+    languages=["PT"],
     input_variant=[
         {"context": "previous_target_next", "include_mwe_segment": True, "transform": "none", "features": []},
-        {"context": "previous_target_next", "include_mwe_segment": True, "transform": "none", "features": ["glosses"]},
-        {"context": "previous_target_next", "include_mwe_segment": True, "transform": "none", "features": ["ner"]},
-        {"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": []},
-        {"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": ["glosses"]},
-        {"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": ["ner"]},
+        #{"context": "previous_target_next", "include_mwe_segment": True, "transform": "none", "features": ["glosses"]},
+        #{"context": "previous_target_next", "include_mwe_segment": True, "transform": "none", "features": ["ner"]},
+        #{"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": []},
+        #{"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": ["glosses"]},
+        #{"context": "previous_target_next", "include_mwe_segment": True, "transform": "highlight", "features": ["ner"]},
 
-        {"context": "target", "include_mwe_segment": True, "transform": "none", "features": []},
-        {"context": "target", "include_mwe_segment": True, "transform": "none", "features": ["glosses"]},
-        {"context": "target", "include_mwe_segment": True, "transform": "none", "features": ["ner"]},
-        {"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": []},
-        {"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": ["glosses"]},
-        {"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": ["ner"]},
+        #{"context": "target", "include_mwe_segment": True, "transform": "none", "features": []},
+        #{"context": "target", "include_mwe_segment": True, "transform": "none", "features": ["glosses"]},
+        #{"context": "target", "include_mwe_segment": True, "transform": "none", "features": ["ner"]},
+        #{"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": []},
+        #{"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": ["glosses"]},
+        #{"context": "target", "include_mwe_segment": True, "transform": "highlight", "features": ["ner"]},
 
     ],
-    model_families=["logreg_tfidf", "logreg_word2vec", "mBERT"],
+    #model_families=["logreg_tfidf", "logreg_word2vec", "mBERT"],
+    model_families=["mBERT"],
     seeds=[51]
 )
